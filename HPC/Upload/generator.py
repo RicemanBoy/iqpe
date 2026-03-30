b138f84634dd6cacd3ae238a82cbe081f7c830d3
Dustin Seboldt <dustin.seboldt@fau.de>
2026-03-30 13:11:49 +0200

    upload steane_noPS again

--- HPC/Upload/generator.py
+++ HPC/Upload/generator.py
@@ -1,4 +1,4 @@
-N=10
+N=400
 
 for i in range(N):
     file=open('{}.py'.format(i), 'a')
@@ -11,7 +11,7 @@ for i in range(N):
     file=open('job{}.script'.format(i),'a')
     file.write('#!/bin/bash -l \n')
     file.write('#SBATCH --ntasks=8 \n')
-    file.write('#SBATCH --time=18:00:00 \n')
+    file.write('#SBATCH --time=12:00:00 \n')
     file.write('#SBATCH --job-name=BScDustin{} \n'.format(i))
     file.write('#SBATCH --export=NONE \n')
     file.write('unset SLURM_EXPORT_ENV \n')
