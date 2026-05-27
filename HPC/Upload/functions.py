73193bf6d6c28905f3405d18f185b5f56f2843d4
Dustin S <dustin.seboldt@fau.de>
2026-05-27 11:37:26 +0200

    mittag!

--- HPC/Upload/functions.py
+++ HPC/Upload/functions.py
@@ -1,34 +1,19 @@
 from classes import *
 
 def gen_data(name):
-    p = np.linspace(0,0.001,10)[7:]
+    p = np.linspace(0,0.007,10)
     y, y_qec = [],[]
     err, err_qec = [], []
 
     for r in p:
-        ok, errr = avg15("steane", 3, noise=r, qec=True,k=1)
+        ok, errr = avg15_repcode(7, 3, r, qec = False, k = 1, bias = -0.9)
+        ok1, errr1 = avg15_repcode(7, 3, r, qec = True, k = 1, bias = -0.9)
+        # ok, errr = avg15("steane", 3, noise=r, qec=True,k=1)
         y.append(ok), err.append(errr)
         # ok1, errr1 = avg15_coin("notsteane", 3, noise=r, qec=True, k=1)
-        # y_qec.append(ok1), err_qec.append(errr1)
+        y_qec.append(ok1), err_qec.append(errr1)
         # y = avg14_coin_success("steane", 3, r, True, k=1)
 
-    #data = np.array((p, y, y_qec, err, err_qec))
-    data = np.array((p, y, err))
-    np.savetxt("FT_steane_PS_qec_7_9{}.txt".format(name), data, delimiter=",")
-    # dustin = RotSurf16q(2)
-    # dustin.h(0)
-    # dustin.x(1)
-
-    # dustin.t(0)
-    # dustin.t(1)
-    # dustin.cnot(0,1)
-    # dustin.tdg(1)
-    # dustin.cnot(0,1)
-    # dustin.sdg(0)
-    # dustin.h(0)
-
-    # dustin.readout(0,1000,0)
-
-    # #data = np.array((p, y, y_qec, err, err_qec))
-    # data = np.array((dustin.zeros, dustin.ones))
-    # np.savetxt("rotsurf16_testtt.txt".format(name), data, delimiter=",")
+    data = np.array((p, y, y_qec, err, err_qec))
+    #data = np.array((p, y, err))
+    np.savetxt("Repcode_d7_biasm0_9{}.txt".format(name), data, delimiter=",")
